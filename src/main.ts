78e4b721103eeb3cf7ff7a2032c758585fbb152e
AlexaDoca58 <alexadominguez049@gmail.com>
2025-12-01 20:51:00 -0600

    Finall

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-// CHANGE: Add the following import
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
-// CHANGE: Call the element loader before the `bootstrapModule` call
 defineCustomElements(window);
 
 platformBrowserDynamic()
